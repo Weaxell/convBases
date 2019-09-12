b3bbe311e6d6ccb88064fc4738e43b743bb60e92
Weaxell <thomascarpentier5@gmail.com>
2019-09-12 15:03:38 +0200

    Conversion en base 10 fonctionnelle

--- conversion.py
+++ conversion.py
@@ -1,20 +1,31 @@
-
 #coding: utf-8
 import os
+import platform
 
-os.system("clear")
+# effacement de l'ecran
+if(platform.system() == 'Windows'):
+    os.system("cls")
+else:
+    os.system("clear")
 
 print("=== Conversion de bases ===")
 valeur = input("Entrez une valeur : ")
 base = int(input("Entrez la base : "))
 baseDest = int(input("Entrez la base de conversion : "))
-print("Longueur :", len(valeur))
 
-exposant = 1
-res = 0
 print("Chaine :", valeur)
+
+
 # conversion en base 10
 i = 0
+res = 0
 while i < len(valeur):
-    print("Valeur au rang", i, ":", valeur[int(i)-1])
+    # print("Valeur au rang", i, ":", int(valeur[i]))
+    # print("    Calcul de", valeur[i], "*", base, "^", len(valeur)-i-1)
+    res = res + int(valeur[i]) * (int(base)**(len(valeur)-i-1))
+    
     i = i + 1
+
+print("=== En base 10, la valeur vaut :", res, "===")
+
+# conversion en base de destination
